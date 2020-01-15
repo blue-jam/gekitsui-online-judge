e6f3cfd9f1a96a787bc9cdcc262cb213ffa38ed1
blue-jam <blue-jam@users.noreply.github.com>
2020-01-15 21:56:31 +0900

    Initial commit

---
+++ src/main/kotlin/bluejam/hobby/gekitsuiwebapp/GekitsuiWebappApplication.kt
@@ -0,0 +1,11 @@
+package bluejam.hobby.gekitsuiwebapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GekitsuiWebappApplication
+
+fun main(args: Array<String>) {
+	runApplication<GekitsuiWebappApplication>(*args)
+}
---
+++ src/test/kotlin/bluejam/hobby/gekitsuiwebapp/GekitsuiWebappApplicationTests.kt
@@ -0,0 +1,13 @@
+package bluejam.hobby.gekitsuiwebapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GekitsuiWebappApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

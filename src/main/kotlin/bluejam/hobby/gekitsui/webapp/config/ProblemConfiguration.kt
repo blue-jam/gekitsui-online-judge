8f72f849aad3c5c9cb6e6c781b51d6e3401aedf6
blue-jam <blue-jam@users.noreply.github.com>
2020-01-18 10:19:58 +0900

    Fix: readAllBytes isn't supported in Heroku

--- src/main/kotlin/bluejam/hobby/gekitsui/webapp/config/ProblemConfiguration.kt
+++ src/main/kotlin/bluejam/hobby/gekitsui/webapp/config/ProblemConfiguration.kt
@@ -6,6 +6,8 @@ import org.springframework.boot.ApplicationRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.io.ResourceLoader
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 @Configuration
 class ProblemConfiguration {
@@ -19,8 +21,8 @@ class ProblemConfiguration {
         aPlusBMod.name = "aplusbmod"
         aPlusBMod.title = "A + B mod 10"
         val fileUrl = resourceLoader.getResource("classpath:problem/aplusbmod.md").url
-        aPlusBMod.statement = fileUrl.openStream().use {
-            String(it.readAllBytes())
+        aPlusBMod.statement = BufferedReader(InputStreamReader(fileUrl.openStream())).use {
+            it.readText()
         }
 
         problemRepository.save(aPlusBMod)

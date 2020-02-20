c472d50e04fa12245e0480994ff00d2c2e4ea91e
blue-jam <blue-jam@users.noreply.github.com>
2020-02-20 21:47:17 +0900

    Add test for accessing index page

--- webapp/src/test/kotlin/bluejam/hobby/gekitsui/webapp/GekitsuiWebappApplicationTests.kt
+++ webapp/src/test/kotlin/bluejam/hobby/gekitsui/webapp/GekitsuiWebappApplicationTests.kt
@@ -1,13 +1,28 @@
 package bluejam.hobby.gekitsui.webapp
 
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class GekitsuiWebappApplicationTests {
+	@Autowired
+	lateinit var mockMvc: MockMvc
 
 	@Test
 	fun contextLoads() {
 	}
 
+	@Test
+	fun `access index page without login`() {
+		mockMvc.perform(MockMvcRequestBuilders.get("/"))
+				.andExpect(MockMvcResultMatchers.status().isOk)
+				.andExpect(MockMvcResultMatchers.view().name("index"))
+				.andExpect(MockMvcResultMatchers.model().attributeDoesNotExist("username"))
+	}
 }

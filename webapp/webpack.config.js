b689885d82d01c0c38cdb8e26586a1c3fa1cff05
blue-jam <blue-jam@users.noreply.github.com>
2020-02-28 23:20:10 +0900

    Add built JS to jar
    - Change webpack config based on mode: https://webpack.js.org/configuration/mode/

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -1,33 +1,44 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/main/js/app.tsx',
-    devtool: 'sourcemaps',
-    mode: 'developement',
-    output: {
-        path: __dirname,
-        filename: './src/main/resources/static/built/bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: path.join(__dirname, '.'),
-                exclude: /(node_modules)/,
-                use: [{
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
-                }]
-            },
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /(node_modules)/
-            },
-        ]
-    },
-    "resolve": {
-        extensions: [ '.tsx', '.ts', '.js' ]
+module.exports = (env, argv) => {
+    const config = {
+        entry: './src/main/js/app.tsx',
+        devtool: 'sourcemaps',
+        mode: 'development',
+        output: {
+            path: path.join(__dirname, 'src/main/resources'),
+            filename: './static/built/bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: path.join(__dirname, '.'),
+                    exclude: /(node_modules)/,
+                    use: [{
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env', '@babel/preset-react']
+                        }
+                    }]
+                },
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /(node_modules)/
+                },
+            ]
+        },
+        "resolve": {
+            extensions: ['.tsx', '.ts', '.js']
+        }
+    };
+
+    if (argv.mode === 'production') {
+        config.devtool = undefined;
+        config.output.path = path.join(__dirname, 'build', 'resources', 'main')
     }
+
+    console.log(config);
+
+    return config;
 };

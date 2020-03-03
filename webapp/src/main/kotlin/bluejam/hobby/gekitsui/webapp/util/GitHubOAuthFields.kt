79d2117f54803b5beaf7d3422936d546f4769a0a
blue-jam <blue-jam@users.noreply.github.com>
2020-03-04 00:56:20 +0900

    Make GitHubOAuthFields object

--- webapp/src/main/kotlin/bluejam/hobby/gekitsui/webapp/util/GitHubOAuthFields.kt
+++ webapp/src/main/kotlin/bluejam/hobby/gekitsui/webapp/util/GitHubOAuthFields.kt
@@ -2,10 +2,8 @@ package bluejam.hobby.gekitsui.webapp.util
 
 import org.springframework.security.oauth2.core.user.OAuth2User
 
-class GitHubOAuthFields {
-    companion object {
-        fun getUserName(oAuth2User: OAuth2User): String? = oAuth2User.attributes["login"] as String?
+object GitHubOAuthFields {
+    fun getUserName(oAuth2User: OAuth2User): String? = oAuth2User.attributes["login"] as String?
 
-        fun getUserId(oAuth2User: OAuth2User): Int? = oAuth2User.attributes["id"] as Int?
-    }
+    fun getUserId(oAuth2User: OAuth2User): Int? = oAuth2User.attributes["id"] as Int?
 }

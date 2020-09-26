4ab9ce362c35956c0985b43e23a86dae39828c24
blue_jam <blue-jam@users.noreply.github.com>
2020-09-27 00:08:30 +0900

    Fix build

--- webapp/src/main/js/contest/ProblemTab.tsx
+++ webapp/src/main/js/contest/ProblemTab.tsx
@@ -32,7 +32,7 @@ const ProblemTab: React.FunctionComponent<Props> = ({
     hljs.initHighlighting();
 
     const MathJaxDocument = MathJax.document(document, {
-      InputJax: new TeX(),
+      InputJax: new TeX(null),
       OutputJax: new CHTML({
         fontURL:
           'https://cdn.jsdelivr.net/npm/mathjax@3/es5/output/chtml/fonts/woff-v2'
